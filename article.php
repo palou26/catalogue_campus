212487d6f77c9ec9ae327e4bd57a8c5d835c2319
pascalpluvinet <ppluvinet@yahoo.fr>
2019-02-04 08:49:42 +0100

    Add php files

---
+++ article.php
@@ -0,0 +1,85 @@
+
+
+
+
+<?php
+
+
+
+$id= 1;
+$article="Sac Noir trop Beau trop cool";
+$prix=50;
+
+?>
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Boutique de sacs">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>BAGS STORE</title>
+</head>
+
+<body>
+<header>
+
+    <div class="container-fluid ">
+        <h1>BAGS STORE</h1>
+    </div>
+
+</header>
+
+<main>
+    <div class="container">
+
+       <div class="row align-items-center">
+
+            <div class="col-md-3">
+                <img src="photos/sac<?php echo $id; ?>.jpg" class="photosac" alt="Photo du Sac<?php echo $id; ?>" title="Photo du Sac<?php echo $id; ?>">
+            </div>
+           <div class="col-md-7">
+               <h2> <?php echo $article; ?></h2>
+           </div>
+
+           <div class="col-md-2  align-items-center">
+               <p class="prix"> <?php echo $prix; ?>€</p>
+           </div>
+
+        </div>
+    </div>
+</main>
+<!-- FOOTER -->
+<footer>
+    <div class="container-fluid">
+
+    </div>
+</footer><!-- FIN FOOTER -->
+
+
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ tuto.php
@@ -0,0 +1,25 @@
+
+
+
+
+<?php
+
+
+    $user = "John";
+    $x = 1;
+    $y = 2;
+    $sum = $x + $y;
+    echo $sum;
+
+
+    $odd_numbers = [1,3,5,7,9];
+    $odd_numbers[5] = 11;
+    print_r($odd_numbers);
+
+; ?>
+<html>
+<head></head>
+    <body>
+    Hello Hello <?php echo "$user tu as $sum ans"; ?>!
+    </body>
+</html>
